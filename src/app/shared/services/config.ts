3e41ff6ab2bd2d5f396e6575b8ffada04c165aa9
Pedro Jorquera <pjorquera@okode.com>
2017-01-16 01:00:05 +0100

    Cosmetics

--- src/app/shared/services/config.ts
+++ src/app/shared/services/config.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActionSheetController } from 'ionic-angular';
+import { ActionSheetController, ToastController } from 'ionic-angular';
 import { Config } from '../models/config';
 import { ConfigInt } from '../models/config-int';
 import { ConfigPre } from '../models/config-pre';
@@ -11,15 +11,25 @@ export class ConfigService {
   private static configs = [ new ConfigInt(), new ConfigPre(), new ConfigPro() ];
   private config: Config;
 
-  constructor(private actionSheetCtrl: ActionSheetController) {
+  constructor(private actionSheetCtrl: ActionSheetController, private toastCtrl: ToastController) {
     this.config = ConfigService.configs[0];
     if (ConfigService.configs.length > 1) {
-      this.actionSheetCtrl.create(
+      let actionSheet = this.actionSheetCtrl.create(
         {
           title: 'Select configuration',
-          buttons: ConfigService.configs.map((c: Config) => ({ text: c.name, handler: () => { this.config = c; } }))
+          buttons: ConfigService.configs.map((c: Config) => (
+            {
+              text: c.name,
+              handler: () =>
+                { 
+                  this.config = c;
+                  actionSheet.dismiss().then(() => { this.toastCtrl.create( { message: `${c.name} selected`, duration: 2000 } ).present(); });
+                  return false;
+                }
+             }))
         }
-      ).present();
+      );
+      actionSheet.present();
     }
   }
 
