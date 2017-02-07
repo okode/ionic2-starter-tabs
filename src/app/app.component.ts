04a8a7b9150ab20136e4484b91803a5e7e344806
alexdiaz <adiaz@okode.com>
2017-02-07 18:35:50 +0100

    Improved configService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { TabsComponent } from './core/components/tabs/tabs';
+import { ConfigService } from './core/services/config';
 
 @Component({
   template: `<ion-nav [root]="rootComponent"></ion-nav>`
@@ -10,12 +11,15 @@ export class App {
 
   rootComponent = TabsComponent;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, configService: ConfigService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+    });
+    configService.ready().then(() => {
+      console.log(`ConfigService ready. Environment: ${configService.getEnvironment()}`);
     });
   }
 }
--- src/app/core/services/config.ts
+++ src/app/core/services/config.ts
@@ -1,62 +1,90 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Http } from '@angular/http';
-import { ActionSheetController, ToastController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+
+type PromiseResolve = (value?: void | PromiseLike<void>) => void;
+type PromiseReject = (reason?: any) => void;
 
 @Injectable()
 export class ConfigService {
 
   private static readonly ENVIRONMENT_KEY = 'configEnvironmentKey';
+  private static readonly CONFIG_JSON_PATH = 'assets/config/config.json';
 
   private environment: string;
   private config: any;
+  private version: string;
+  private isReady = false;
+  private readyPromiseResolve: PromiseResolve;
+  private readyPromiseReject: PromiseReject;
 
   constructor(
     private storage: Storage,
     private http: Http,
-    private actionSheetCtrl: ActionSheetController,
-    private toastCtrl: ToastController)
-  {
+    private actionSheetCtrl: ActionSheetController) {}
+
+  ready() {
+    if(this.isReady) return Promise.resolve();
+    this.initConfigs();
+    return new Promise<void>((resolve, reject) => {
+      this.readyPromiseResolve = resolve;
+      this.readyPromiseReject = reject;
+    });
+  }
+
+  private initConfigs() {
     this.http
-      .get('assets/config/config.json')
-      .subscribe(res => {
-        let configs = res.json();
-        this.setConfig(false, 'default', configs['default']);
-        let environments = Object.keys(configs).filter(environment => environment !== 'default');
-        this.storage.ready().then(() => {
-          this.storage.get(ConfigService.ENVIRONMENT_KEY).then(storedEnvironment => {
-            if (storedEnvironment == null) {
-              console.log('No saved environment detected, will prompt user for selection');
-              if (environments.length > 1) {
-                let actionSheet = this.actionSheetCtrl.create(
-                  {
-                    title: 'Select environment',
-                    enableBackdropDismiss: false,
-                    buttons: environments.map(environment => (
-                      {
-                        text: environment,
-                        handler: () =>
-                          {
-                            this.setConfig(true, environment, configs['default'], configs[environment]);
-                            actionSheet.dismiss().then(() => { this.toastCtrl.create( { message: `${environment} selected`, duration: 2000, position: 'top' } ).present(); });
-                            return false;
-                          }
-                      }))
+      .get(ConfigService.CONFIG_JSON_PATH)
+      .subscribe(
+        res => {
+          let configs = res.json();
+          if (configs == null || Object.keys(configs).length == 0) {
+            this.readyPromiseReject();
+            console.log(`ConfigService fails: '${ConfigService.CONFIG_JSON_PATH}' is empty or invalid`);
+            return;
+          }
+          let environments = Object.keys(configs).filter(environment => environment !== 'default');
+          if (environments == null || environments.length == 0) {
+            this.setConfig(false, 'default', configs['default']);
+          } else {
+            this.storage.ready().then(() => {
+              this.storage.get(ConfigService.ENVIRONMENT_KEY).then(storedEnvironment => {
+                if (storedEnvironment == null) {
+                  console.log('No saved environment detected, will prompt user for selection');
+                  if (environments.length > 1) {
+                    this.showEnvironmentActionSheet(environments, configs);
+                  } else {
+                    this.setConfig(true, environments[0], configs['default'], configs[0]);
                   }
-                );
-                actionSheet.present();
-              } else {
-                if (environments.length == 1) {
-                  this.setConfig(true, environments[0], configs['default'], configs[0]);
+                } else {
+                  console.log(`Detected saved environment: ${storedEnvironment}`);
+                  this.setConfig(false, storedEnvironment, configs['default'], configs[storedEnvironment]);
                 }
-              }
-            } else {
-              console.log(`Detected saved environment: ${storedEnvironment}`);
-              this.setConfig(false, storedEnvironment, configs['default'], configs[storedEnvironment]);
-            }
-          });
-        });
-      });
+              });
+            });
+          }
+        },
+        err => {
+          this.readyPromiseReject();
+          console.log(`ConfigService fails: Not found '${ConfigService.CONFIG_JSON_PATH}'`);
+        }
+      );
+  }
+
+  private showEnvironmentActionSheet(environments, configs) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select environment',
+      enableBackdropDismiss: false,
+      buttons: environments.map(environment => ({
+          text: environment,
+          handler: () => {
+            this.setConfig(true, environment, configs['default'], configs[environment]);
+          }
+        })
+      )
+    });
+    actionSheet.present();
   }
 
   private setConfig(save: boolean, environment: string, configBase: any, configEnvironment?: any) {
@@ -64,12 +92,14 @@ export class ConfigService {
     this.environment = environment;
     this.config = configBase;
     if (configEnvironment != null) {
-      Object.assign(this.config, configEnvironment);    
+      Object.assign(this.config, configEnvironment);
     }
     if (save) {
       console.log(`Saving environment: ${environment}`);
       this.storage.set(ConfigService.ENVIRONMENT_KEY, environment);
     }
+    this.isReady = true;
+    this.readyPromiseResolve();
   }
 
   getConfig() {
@@ -81,14 +111,13 @@ export class ConfigService {
   }
 
   getVersion() {
-    let version = '';
-    if (this.config) {
-      version = this.config.version;
-      if (this.config.versionEnv && this.config.versionEnv != '') {
-        version = `${version}-${this.config.versionEnv}`;
-      }
+    if (this.version) return this.version;
+    if (!this.config) return null;
+    this.version = this.config.version;
+    if (this.config.versionEnv && this.config.versionEnv != '') {
+      this.version = `${this.version}-${this.config.versionEnv}`;
     }
-    return version;
+    return this.version;
   }
 
 }
